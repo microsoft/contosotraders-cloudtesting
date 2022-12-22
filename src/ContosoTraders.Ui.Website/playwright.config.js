2fa616ff18b0fea5d8c1c4dc0b3f4ded2611de94
gokul <gokul.sm@seqato.com>
2022-12-22 20:15:02 +0530

    playwright test issue

--- src/ContosoTraders.Ui.Website/playwright.config.js
+++ src/ContosoTraders.Ui.Website/playwright.config.js
@@ -6,7 +6,8 @@ const { devices } = require('@playwright/test');
  * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
-
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 /**
  * @see https://playwright.dev/docs/test-configuration
