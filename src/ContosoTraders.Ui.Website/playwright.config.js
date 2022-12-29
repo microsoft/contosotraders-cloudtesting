1c5b6a8d2cbd6a87934485fa3e9f442d2fc31642
gokul <gokul.sm@seqato.com>
2022-12-29 23:47:29 +0530

    playwright issue

--- src/ContosoTraders.Ui.Website/playwright.config.js
+++ src/ContosoTraders.Ui.Website/playwright.config.js
@@ -16,7 +16,7 @@ dotenv.config()
 const config = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
