05cbe74b9cf3d19b97253ca88f219c5a337a9704
mithun shanbhag <mithunshanbhag@hotmail.com>
2022-12-30 12:52:13 +0530

    Support operation cancellation

--- src/ContosoTraders.Api.Core/Repositories/Implementations/CosmosGenericRepositoryBase.cs
+++ src/ContosoTraders.Api.Core/Repositories/Implementations/CosmosGenericRepositoryBase.cs
@@ -17,11 +17,15 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
 
     public async Task<IEnumerable<TEntity>> QueryAsync(string querySpec, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         return await ExecuteQueryAsync(querySpec, cancellationToken);
     }
 
     public async Task<IEnumerable<TEntity>> ListAsync(string filterClause = default, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         var querySpec = "select * from c";
 
         if (!string.IsNullOrWhiteSpace(filterClause)) querySpec = $"{querySpec} where {filterClause}";
@@ -31,6 +35,8 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
 
     public async Task<TEntity> GetAsync(string partitionKey, string id, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         try
         {
             var response = await CosmosDatabase
@@ -47,6 +53,8 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
 
     public async Task AddAsync(string partitionKey, TEntity entity, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         await CosmosDatabase
             .GetContainer(ContainerName)
             .CreateItemAsync(entity, new PartitionKey(partitionKey), cancellationToken: cancellationToken);
@@ -54,6 +62,8 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
 
     public async Task UpsertAsync(string partitionKey, TEntity entity, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         await CosmosDatabase
             .GetContainer(ContainerName)
             .UpsertItemAsync(entity, new PartitionKey(partitionKey), cancellationToken: cancellationToken);
@@ -61,6 +71,8 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
 
     public async Task DeleteAsync(string partitionKey, string id, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         await CosmosDatabase
             .GetContainer(ContainerName)
             .DeleteItemAsync<TEntity>(id, new PartitionKey(partitionKey), cancellationToken: cancellationToken);
@@ -68,6 +80,8 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
 
     private async Task<IEnumerable<TEntity>> ExecuteQueryAsync(string querySpec, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         using var queryIterator = CosmosDatabase
             .GetContainer(ContainerName)
             .GetItemQueryIterator<TEntity>(new QueryDefinition(querySpec));
@@ -75,6 +89,8 @@ public abstract class CosmosGenericRepositoryBase<TEntity> : ICosmosGenericRepos
         var results = new List<TEntity>();
         while (queryIterator.HasMoreResults)
         {
+            cancellationToken.ThrowIfCancellationRequested();
+
             var response = await queryIterator.ReadNextAsync(cancellationToken);
             results.AddRange(response.ToList());
         }
