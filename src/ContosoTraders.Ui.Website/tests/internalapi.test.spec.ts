8327286e96712b05e94cc950de1f3281eaa0556d
Marcus Felling <marcus.felling@gmail.com>
2023-01-19 11:10:57 -0600

    convert tests to typescript

--- src/ContosoTraders.Ui.Website/tests/internalapi.test.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/internalapi.test.spec.ts
@@ -1,36 +1,38 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
 let _productid = 1;
-test.beforeEach(async({page})=>{
-  await page.goto('/');
+test.beforeEach(async ({ page }) => {
+    await page.goto('/');
 })
+
 //Test all the APIs in the application
 test.describe('APIs', () => {
     //text serach API
     test('should be able to load search by text data', async ({ request }) => {
-      const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/search/laptops`);
-      expect(response.status()).toBe(200);
-      expect(response.ok()).toBeTruthy();
+        const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/search/laptops`);
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
     });
-  
+
     //load products list
     test('should be able to load products list', async ({ request }) => {
-      const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/?type=laptops`);
-      expect(response.status()).toBe(200);
-      expect(response.ok()).toBeTruthy();
+        const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/?type=laptops`);
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
     });
-  
+
     //load product details
     test('should be able to load product details', async ({ request }) => {
-      const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/`+_productid);
-      expect(response.status()).toBe(200);
-      expect(response.ok()).toBeTruthy();
+        const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/` + _productid);
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
     });
-  
+
     //filter products by brands
     test('should be able to filter products by brands', async ({ request }) => {
-      const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/?type=laptops&brand=1`);
-      expect(response.status()).toBe(200);
-      expect(response.ok()).toBeTruthy();
+        const response = await request.get(`${process.env.REACT_APP_APIURL}/Products/?type=laptops&brand=1`);
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
     });
 
     //filter products by category - Controller and then assert category
@@ -247,4 +249,4 @@ test.describe('APIs', () => {
         //assert count of category - monitors
         expect(productsFromResponse.length).toBe(total_no_of_monitors)
     });
-  });
+});
