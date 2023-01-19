3eb2fcbffdaccd64480827b0704d931169630006
Marcus Felling <marcus.felling@gmail.com>
2023-01-19 15:02:52 -0600

    timeout

--- src/ContosoTraders.Ui.Website/playwright.config.ts
+++ src/ContosoTraders.Ui.Website/playwright.config.ts
@@ -9,7 +9,7 @@ require('dotenv').config()
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 40 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
