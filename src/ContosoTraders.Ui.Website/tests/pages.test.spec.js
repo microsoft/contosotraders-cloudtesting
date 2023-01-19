bf79e3a4f8c70e8b4c57572ff55ef663f2333b82
gokulsm <107677159+gokulsm@users.noreply.github.com>
2023-01-19 21:59:43 +0530

    IFrame testing and File Upload testing, fixes #45 #80 (#84)

--- src/ContosoTraders.Ui.Website/tests/pages.test.spec.js
+++ src/ContosoTraders.Ui.Website/tests/pages.test.spec.js
@@ -10,33 +10,6 @@ test.beforeAll(async ({ browser })=>{
 test.beforeEach(async({page})=>{
   await page.goto('/');
 })
-//#region Uncomment below lines to run dark mode tests
-// test.describe('Dark Mode', () => {
-//   test('shows page in dark mode', async ({ page }) => {
-//     await page.locator('input.MuiSwitch-input').check()
-//     await expect(page.locator('.App')).toHaveAttribute('class', 'App dark')
-//   })
-// });
-//#endregion
-test('Test with geolocation', async ({ page, context, request }) => {
-  const ipTest = await request.get(`${process.env.REACT_APP_GEOLOCATIONAPI}`);
-  expect(ipTest.status()).toBe(200);
-  expect(ipTest.ok()).toBeTruthy();
-  const location = JSON.parse(await ipTest.text())
-
-  const latitude = location.latitude//await page.locator('input#latitude').inputValue();
-  const longitude = location.longitude//await page.locator('input#longitude').inputValue();
-  const point = latitude+','+longitude;
-  const response = await request.get(`${process.env.REACT_APP_GEOAPIBASEURL}/Locations/${point}?key=${process.env.REACT_APP_BINGMAPSKEY}`);
-  expect(response.status()).toBe(200);
-  expect(response.ok()).toBeTruthy();
-  if(latitude != null && longitude != null){
-    await context.setGeolocation({ longitude: parseFloat(longitude), latitude: parseFloat(latitude) });
-  }
-  await Promise.all([
-    page.waitForSelector('#current-location'),
-  ]);
-});
 
 test.describe('Header', () => {
   test('should be able to search by text', async ({ page }) => {
