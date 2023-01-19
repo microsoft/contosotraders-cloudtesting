7edad62ff26c8600d1600e8f1161b681bc7fd5d6
Marcus Felling <marcus.felling@gmail.com>
2023-01-19 16:10:59 -0600

    zoom in, zoom out

--- src/ContosoTraders.Ui.Website/tests/features.test.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/features.test.spec.ts
@@ -1,7 +1,7 @@
 //Tests some features like third-party API , Dark mode, Geolocation, Iframe testing etc
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async({page})=>{
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
@@ -16,29 +16,28 @@ test.beforeEach(async({page})=>{
 
 //Geolocation Testing
 test('Test with geolocation', async ({ page, context, request }) => {
-    const ipTest = await request.get(`${process.env.REACT_APP_GEOLOCATIONAPI}`);
-    expect(ipTest.status()).toBe(200);
-    expect(ipTest.ok()).toBeTruthy();
-    const location = JSON.parse(await ipTest.text())
-  
-    const latitude = location.latitude//await page.locator('input#latitude').inputValue();
-    const longitude = location.longitude//await page.locator('input#longitude').inputValue();
-    const point = latitude+','+longitude;
-    const response = await request.get(`${process.env.REACT_APP_GEOAPIBASEURL}/Locations/${point}?key=${process.env.REACT_APP_BINGMAPSKEY}`);
-    expect(response.status()).toBe(200);
-    expect(response.ok()).toBeTruthy();
-    if(latitude != null && longitude != null){
-      await context.setGeolocation({ longitude: parseFloat(longitude), latitude: parseFloat(latitude) });
-    }
-    await Promise.all([
-      page.waitForSelector('#current-location'),
-    ]);
+  const ipTest = await request.get(`${process.env.REACT_APP_GEOLOCATIONAPI}`);
+  expect(ipTest.status()).toBe(200);
+  expect(ipTest.ok()).toBeTruthy();
+  const location = JSON.parse(await ipTest.text())
+  const latitude = location.latitude
+  const longitude = location.longitude
+  const point = latitude + ',' + longitude;
+  const response = await request.get(`${process.env.REACT_APP_GEOAPIBASEURL}/Locations/${point}?key=${process.env.REACT_APP_BINGMAPSKEY}`);
+  expect(response.status()).toBe(200);
+  expect(response.ok()).toBeTruthy();
+  if (latitude != null && longitude != null) {
+    await context.setGeolocation({ longitude: parseFloat(longitude), latitude: parseFloat(latitude) });
+  }
+  await Promise.all([
+    page.waitForSelector('#current-location'),
+  ]);
 });
 
 //Iframe Testing
 test('Test with Iframes', async ({ page }) => {
   await page.goto('/');
   await page.mouse.wheel(0, 15000);
-  await page.frameLocator('internal:attr=[title="geolocation"i]').getByRole('button', { name: 'Zoom In' }).hover();
   await page.frameLocator('internal:attr=[title="geolocation"i]').getByRole('button', { name: 'Zoom In' }).click();
+  await page.frameLocator('internal:attr=[title="geolocation"i]').getByRole('button', { name: 'Zoom Out' }).click();
 });
