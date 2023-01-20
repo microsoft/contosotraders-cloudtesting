585e9a28d11f07308074cd2b7bf89be07152c886
gokulsm <107677159+gokulsm@users.noreply.github.com>
2023-01-20 12:22:29 +0530

    updates to iframe tests

--- src/ContosoTraders.Ui.Website/tests/fileupload.test.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/fileupload.test.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+const imgPath = require('path').resolve(__dirname, '..')+'/src/assets/images/original/Contoso_Assets/Grid_Products_Collection/product_image.png';
 
 test.beforeEach(async({page})=>{
   await page.goto('/');
@@ -8,7 +9,7 @@ test.describe('FileUpload', () => {
     test("Upload a file", async ({ page }) => {      
         await page.goto("/");
         await page.getByRole('button', { name: 'iconimage' }).click();
-        await page.locator('input[type="file"]').setInputFiles(__dirname+'/product_image.png')
+        await page.locator('input[type="file"]').setInputFiles(imgPath);
         await expect(page).toHaveURL('/suggested-products-list');
     });
 });
