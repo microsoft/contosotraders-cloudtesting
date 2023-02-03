dc96a5285ef3dfe3cca2374c6906e03adcf81c8a
mithun shanbhag <mithunshanbhag@hotmail.com>
2023-02-03 12:18:49 +0530

    non-null assertion

--- src/ContosoTraders.Ui.Website/tests/account.ts
+++ src/ContosoTraders.Ui.Website/tests/account.ts
@@ -16,9 +16,9 @@ test.beforeAll(async ({ browser }) => {
         const dialogPromise = page.waitForEvent('popup');
         await page.getByText('LOGIN').click();
         const dialog = await dialogPromise;
-        await dialog.getByPlaceholder('Email, phone, or Skype').fill(process.env.AADUSERNAME);
+        await dialog.getByPlaceholder('Email, phone, or Skype').fill(process.env.AADUSERNAME!);
         await dialog.getByRole('button', { name: 'Next' }).click();
-        await dialog.getByPlaceholder('Password').fill(process.env.AADPASSWORD);
+        await dialog.getByPlaceholder('Password').fill(process.env.AADPASSWORD!);
         await dialog.getByRole('button', { name: 'Sign in' }).click();
         // Do not stay signed in
         await dialog.getByRole('button', { name: 'No' }).click();
