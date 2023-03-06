33a63b249b6e8acf34771483be0b0e70c2a3aefb
Marcus Felling <marcus.felling@gmail.com>
2023-03-06 17:14:43 -0600

    use defineConfig method, add webkit

--- src/ContosoTraders.Ui.Website/playwright.config.ts
+++ src/ContosoTraders.Ui.Website/playwright.config.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { devices, PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -7,10 +7,10 @@ import { devices, PlaywrightTestConfig } from '@playwright/test';
  */
 require('dotenv').config()
 
-const config: PlaywrightTestConfig = {
+export default defineConfig({
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -73,8 +73,12 @@ const config: PlaywrightTestConfig = {
       use: {
         ...devices['Desktop Firefox']
       },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari']
+      },
     }
   ],
-};
-
-module.exports = config;
+});
