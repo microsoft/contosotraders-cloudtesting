0d80cc252c8d6fd09e1ffbe78f75fd60cf1f8c16
Marcus Felling <marcus.felling@gmail.com>
2023-03-09 13:43:38 -0600

    remove webkit, TOO FLAKY!!

--- src/ContosoTraders.Ui.Website/playwright.config.ts
+++ src/ContosoTraders.Ui.Website/playwright.config.ts
@@ -73,12 +73,6 @@ export default defineConfig({
       use: {
         ...devices['Desktop Firefox']
       },
-    },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari']
-      },
     }
   ],
 });
