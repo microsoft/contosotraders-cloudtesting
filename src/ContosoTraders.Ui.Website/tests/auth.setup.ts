b7a0fe7a35c6578d86919eb8db969ae542435c80
Mithun Shanbhag <mithunshanbhag@users.noreply.github.com>
2023-04-18 12:25:23 +0530

    Conditionally disable auth test (#215)
    Change description:
    * Conditional disabling of auth tests
    * REACT_APP_ prefix for env vars

--- src/ContosoTraders.Ui.Website/tests/auth.setup.ts
+++ src/ContosoTraders.Ui.Website/tests/auth.setup.ts
@@ -4,16 +4,16 @@ const authFile = '.auth/user.json';
 
 setup('authenticate', async ({ browser }) => {
     // skip setup if environment variables for AAD creds are not set
-    setup.skip(process.env.AADUSERNAME === undefined || process.env.AADPASSWORD === undefined, 'AADUSERNAME and AADPASSWORD environment variables must be set');
+    setup.skip(process.env.REACT_APP_AADUSERNAME === undefined || process.env.REACT_APP_AADPASSWORD === undefined, 'AADUSERNAME and AADPASSWORD environment variables must be set');
     const page = await browser.newPage();
     await page.goto('');
     // Sign in using creds from env variables
     const dialogPromise = page.waitForEvent('popup');
     await page.getByText('LOGIN').click();
     const dialog = await dialogPromise;
-    await dialog.getByPlaceholder('Email, phone, or Skype').fill(process.env.AADUSERNAME!);
+    await dialog.getByPlaceholder('Email, phone, or Skype').fill(process.env.REACT_APP_AADUSERNAME!);
     await dialog.getByRole('button', { name: 'Next' }).click();
-    await dialog.getByPlaceholder('Password').fill(process.env.AADPASSWORD!);
+    await dialog.getByPlaceholder('Password').fill(process.env.REACT_APP_AADPASSWORD!);
     await dialog.getByRole('button', { name: 'Sign in' }).click();
     // Do not stay signed in
     await dialog.getByRole('button', { name: 'No' }).click();
