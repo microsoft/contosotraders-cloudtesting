aec5abe64ec36335ebf50091fee8d0e542b27fad
Marcus Felling <marcus.felling@gmail.com>
2023-04-24 14:31:02 -0500

    test: update to 1.32.3, add windows snapshots, dark mode assert, format

--- src/ContosoTraders.Ui.Website/tests/darkmode.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/darkmode.spec.ts
@@ -8,8 +8,10 @@ test.describe.skip('Dark Mode', () => {
   test('should be able to toggle dark mode', async ({ page }) => {
     await page.getByLabel('Dark Mode').check();
     await expect(page.locator('.App')).toHaveAttribute('class', 'App dark')
+    await expect(page.locator('.App')).toHaveCSS('background-color', 'rgb(34, 34, 34)')
     await page.getByLabel('Dark Mode').uncheck();
     await expect(page.locator('.App')).toHaveAttribute('class', 'App light')
+    await expect(page.locator('.App')).toHaveCSS('background-color', 'rgba(0, 0, 0, 0)')
   })
   test('verify dark mode is pixel perfect - on', async ({ page }) => {
     await page.getByLabel('Dark Mode').check();
