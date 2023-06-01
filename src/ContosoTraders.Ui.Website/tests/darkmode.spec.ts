86b056d663a1efbf0d26c1782541429ab67e03f9
Marcus Felling <marcus.felling@gmail.com>
2023-06-01 10:06:52 -0500

    only run vrt on chromium (fixed)

--- src/ContosoTraders.Ui.Website/tests/darkmode.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/darkmode.spec.ts
@@ -4,7 +4,8 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
-test.describe.skip('Dark Mode', () => {
+test.describe('Dark Mode', () => {
+  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
   test('should be able to toggle dark mode', async ({ page }) => {
     await page.getByLabel('Dark Mode').check();
     await expect(page.locator('.App')).toHaveAttribute('class', 'App dark')
@@ -14,12 +15,10 @@ test.describe.skip('Dark Mode', () => {
     await expect(page.locator('.App')).toHaveCSS('background-color', 'rgba(0, 0, 0, 0)')
   })
   test('verify dark mode is pixel perfect - on', async ({ page }) => {
-    test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
     await page.getByLabel('Dark Mode').check();
     await expect(page).toHaveScreenshot();
   })
   test('verify dark mode is pixel perfect - off', async ({ page }) => {
-    test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
     await expect(page).toHaveScreenshot();
   })
 });
--- src/ContosoTraders.Ui.Website/tests/pages.spec.ts
+++ src/ContosoTraders.Ui.Website/tests/pages.spec.ts
@@ -59,14 +59,15 @@ test.describe('Carousel', () => {
     await page.getByTestId('carousel').getByRole('button', { name: 'More Details' }).first().click();
     await expect(page).toHaveURL('/list/controllers');
   })
+});
 
+test.describe('CarouselVRT', () => {  
+  test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
   test('verify carousel is pixel perfect - slide 1', async ({ page }) => {
-    test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
     await expect(page.getByTestId('carousel')).toHaveScreenshot();
   })
 
   test('verify carousel is pixel perfect - slide 2', async ({ page }) => {
-    test.skip(({ browserName }) => browserName !== 'chromium', 'Chromium only!');
     const carousel = page.getByTestId('carousel');
     await carousel.getByRole('button', { name: 'Next' }).click();
     await expect(carousel).toHaveScreenshot();
