85a923ebd7c993f9f1325b5fcc9f70b21a8853d9
Marcus Felling <marcus.felling@gmail.com>
2023-06-01 10:53:21 -0500

    temp enable dark mode to generate linux images

--- src/ContosoTraders.Ui.Website/src/components/header/appbar.js
+++ src/ContosoTraders.Ui.Website/src/components/header/appbar.js
@@ -2,9 +2,7 @@ import React, { useCallback, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { AppBar, InputAdornment, TextField, Button } from '@mui/material';
-//#region Uncomment below lines to run dark mode tests
-// import {FormGroup, FormControlLabel, Switch } from '@mui/material';
-//#endregion
+import {FormGroup, FormControlLabel, Switch } from '@mui/material';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
@@ -367,11 +365,9 @@ function TopAppBar(props) {
               <img src={BagIcon} alt="iconimage" />
             </Badge>
           </IconButton>
-          {/* #region Uncomment below lines to run dark mode tests */}
-          {/* <FormGroup className='theme-class'>
+          <FormGroup className='theme-class'>
             <FormControlLabel labelPlacement="start" control={<Switch aria-label='theme change' id="theme" color="primary" onChange={(e) => props.handleThemeChange(e.target.checked)}/>} label="Dark Mode" />
-          </FormGroup> */}
-          {/* #endregion */}
+          </FormGroup>
           <div className={`sectionMobile d-block d-md-none d-lg-none`}>
             <IconButton
               aria-label="show more"
